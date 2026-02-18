05d43ff4e1989f79e41739618d77c9f0c17ef773
Leonardo Barros <115377071+Barrosleo@users.noreply.github.com>
2026-02-18 12:24:50 +0000

    Update score.py

--- src/score.py
+++ src/score.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 import joblib
 import pandas as pd
 
-app = FastAPI()
+app = FastAPI(
+    title="Hybrid Cloud Threat Prediction API",
+    description="Machine Learning API for predicting cyber threats in hybrid cloud environments.",
+    version="1.0.0"
+)
 
 # Load model at startup
 model = joblib.load("models/random_forest.pkl")
@@ -13,8 +17,19 @@ def home():
 
 @app.post("/predict")
 def predict(data: dict):
+    """
+    Accepts JSON input and returns a prediction.
+    Example input:
+    {
+      "Dur": 0.12,
+      "Proto_encoded": 1,
+      "SrcPort": 443,
+      "DstPort": 51514,
+      "Packets": 10,
+      "Bytes": 850
+    }
+    """
     df = pd.DataFrame([data])
     prediction = model.predict(df)[0]
     label = "Attack" if prediction == 1 else "Normal"
     return {"prediction": int(prediction), "label": label}
-
